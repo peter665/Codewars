520219fdd0530724753af93192a4ae2c40f75a12
peter <peto.mester@gmail.com>
2025-01-13 22:03:10 +0100

    finished, second solution

--- Equal Sides of an Array/equal_sides.py
+++ Equal Sides of an Array/equal_sides.py
@@ -18,7 +18,7 @@
 
 def find_even_index(arr):
     for i in range(len(arr)):
-        if sum([x for x in arr[:i]]) == sum([x for x in arr[i+1:]]):
+        if sum(arr[:i]) == sum(arr[i+1:]):
             return i
     return -1
 
